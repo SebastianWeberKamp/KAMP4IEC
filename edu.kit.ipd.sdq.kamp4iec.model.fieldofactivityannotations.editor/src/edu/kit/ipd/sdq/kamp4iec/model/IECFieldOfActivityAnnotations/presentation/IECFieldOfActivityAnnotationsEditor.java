6052fba3b7a77e1ba64dc365313e203ad1fb9d9e
Jannis Raetz <Jannis@DESKTOP-G76S098.localdomain>
2017-12-12 10:42:16 +0100

    Alte generierte KLassen entfernt.

--- edu.kit.ipd.sdq.kamp4iec.model.fieldofactivityannotations.editor/src/edu/kit/ipd/sdq/kamp4iec/model/IECFieldOfActivityAnnotations/presentation/IECFieldOfActivityAnnotationsEditor.java
+++ edu.kit.ipd.sdq.kamp4iec.model.fieldofactivityannotations.editor/src/edu/kit/ipd/sdq/kamp4iec/model/IECFieldOfActivityAnnotations/presentation/IECFieldOfActivityAnnotationsEditor.java
@@ -456,6 +456,8 @@ public class IECFieldOfActivityAnnotationsEditor
 	 */
 	protected EContentAdapter problemIndicationAdapter =
 		new EContentAdapter() {
+			protected boolean dispatching;
+
 			@Override
 			public void notifyChanged(Notification notification) {
 				if (notification.getNotifier() instanceof Resource) {
@@ -471,21 +473,26 @@ public class IECFieldOfActivityAnnotationsEditor
 							else {
 								resourceToDiagnosticMap.remove(resource);
 							}
-
-							if (updateProblemIndication) {
-								getSite().getShell().getDisplay().asyncExec
-									(new Runnable() {
-										 public void run() {
-											 updateProblemIndication();
-										 }
-									 });
-							}
+							dispatchUpdateProblemIndication();
 							break;
 						}
 					}
 				}
 				else {
 					super.notifyChanged(notification);
+				}
+			}
+
+			protected void dispatchUpdateProblemIndication() {
+				if (updateProblemIndication && !dispatching) {
+					dispatching = true;
+					getSite().getShell().getDisplay().asyncExec
+						(new Runnable() {
+							 public void run() {
+								 dispatching = false;
+								 updateProblemIndication();
+							 }
+						 });
 				}
 			}
 
@@ -498,14 +505,7 @@ public class IECFieldOfActivityAnnotationsEditor
 			protected void unsetTarget(Resource target) {
 				basicUnsetTarget(target);
 				resourceToDiagnosticMap.remove(target);
-				if (updateProblemIndication) {
-					getSite().getShell().getDisplay().asyncExec
-						(new Runnable() {
-							 public void run() {
-								 updateProblemIndication();
-							 }
-						 });
-				}
+				dispatchUpdateProblemIndication();
 			}
 		};
 
@@ -703,14 +703,11 @@ public class IECFieldOfActivityAnnotationsEditor
 			}
 
 			if (markerHelper.hasMarkers(editingDomain.getResourceSet())) {
-				markerHelper.deleteMarkers(editingDomain.getResourceSet());
-				if (diagnostic.getSeverity() != Diagnostic.OK) {
-					try {
-						markerHelper.createMarkers(diagnostic);
-					}
-					catch (CoreException exception) {
-						IECFieldOfActivityAnnotationsEditorPlugin.INSTANCE.log(exception);
-					}
+				try {
+					markerHelper.updateMarkers(diagnostic);
+				}
+				catch (CoreException exception) {
+					IECFieldOfActivityAnnotationsEditorPlugin.INSTANCE.log(exception);
 				}
 			}
 		}
@@ -1117,6 +1114,7 @@ public class IECFieldOfActivityAnnotationsEditor
 
 				selectionViewer = (TreeViewer)viewerPane.getViewer();
 				selectionViewer.setContentProvider(new AdapterFactoryContentProvider(adapterFactory));
+				selectionViewer.setUseHashlookup(true);
 
 				selectionViewer.setLabelProvider(new AdapterFactoryLabelProvider(adapterFactory));
 				selectionViewer.setInput(editingDomain.getResourceSet());
@@ -1422,6 +1420,7 @@ public class IECFieldOfActivityAnnotationsEditor
 
 					// Set up the tree viewer.
 					//
+					contentOutlineViewer.setUseHashlookup(true);
 					contentOutlineViewer.setContentProvider(new AdapterFactoryContentProvider(adapterFactory));
 					contentOutlineViewer.setLabelProvider(new AdapterFactoryLabelProvider(adapterFactory));
 					contentOutlineViewer.setInput(editingDomain.getResourceSet());
@@ -1569,7 +1568,9 @@ public class IECFieldOfActivityAnnotationsEditor
 					// Save the resources to the file system.
 					//
 					boolean first = true;
-					for (Resource resource : editingDomain.getResourceSet().getResources()) {
+					List<Resource> resources = editingDomain.getResourceSet().getResources();
+					for (int i = 0; i < resources.size(); ++i) {
+						Resource resource = resources.get(i);
 						if ((first || !resource.getContents().isEmpty() || isPersisted(resource)) && !editingDomain.isReadOnly(resource)) {
 							try {
 								long timeStamp = resource.getTimeStamp();
